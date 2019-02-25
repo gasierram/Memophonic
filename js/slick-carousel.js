702b08889496ea207302aae24bb3ad48a51abfae
Alejandro Sierra <gasierram@unal.edu.co>
2019-02-25 05:08:11 -0500

    versión parcial faltan ajustes

--- js/slick-carousel.js
+++ js/slick-carousel.js
@@ -1,5 +1,40 @@
-// $(document).ready(function(){
-//     $('.your-class').slick({
-//       setting-name: setting-value
-//     });
-//   });
+$(function() { var logo = $(".lrg-logo"); $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    
+        if (scroll >= 500) {
+          if(!logo.hasClass("sml-logo")) {
+            logo.hide();
+            logo.removeClass('lrg-logo').addClass("sml-logo").fadeIn( "slow");
+          }
+        } else {
+          if(!logo.hasClass("lrg-logo")) {
+            logo.hide();
+            logo.removeClass("sml-logo").addClass('lrg-logo').fadeIn( "slow");
+          }
+        }
+    
+    });
+    });
+
+    var image = document.getElementsByClassName(".banner-area");
+    var currentPos = 0;
+    var images = ["../images/home/web_1.jpg", "../images/home/web_14.jpg", "../images/home/web_20.jpg"]
+
+    function volgendefoto() {
+        if (++currentPos >= images.length)
+            currentPos = 0;
+
+        
+        image.style.backgroundImage = "url('images[currentPos]')";
+        // $(".banner-area").css("background-image", 'url("' + images[currentPos] + '")'  );
+        // console.log('url("' + imageUrl + '")' )
+        // $(".banner-area").css("background-position", 'center'  );
+
+        // $(".banner-area").css("-webkit-background-size", 'cover'  );
+        // $(".banner-area").css("background-size", 'cover'  );
+        // $(".banner-area").css("position", 'relative'  );
+        // $(".banner-area").css("height", '100%'  );
+
+    }
+
+    setInterval(volgendefoto, 3000);
